00df1bee5a31f407010734a19a83600d6f93388c
James Hughff <james.lee.hughff@gmail.com>
2026-03-30 13:37:29 +0100

    fix: fixed race condition in middleware

--- main/middleware.py
+++ main/middleware.py
@@ -1,5 +1,5 @@
+from django.db.models import F
 from . import models
-from datetime import date
 
 NO_RECORD = ("session/", "_cart", "upload_", "lookup_", "get_", "delete_", "_payment", "analytics/", "favicon", "jsonws", "_ignition")
 
@@ -7,31 +7,22 @@ def analytics(get_response):
 
     def middleware(request):
         response = get_response(request)
-        
-        #Page views counter
+
         page = request.path
-        
-        #Check if page is non-user page
+
         for value in NO_RECORD:
             if value in page:
                 return response
 
-        try:
-            page_model = models.Views.objects.get(name=page)
-            page_model.views += 1
-            page_model.save()
-        except models.Views.DoesNotExist:
-            page_model = models.Views.objects.create(name=page, views=1)
-            page_model.save()
+        models.Views.objects.update_or_create(
+            name=page,
+            defaults={'views': F('views') + 1},
+            create_defaults={'views': 1}
+        )
 
-
-        #Session counter
         session_id = request.COOKIES.get("sessionid")
-        try:
-            models.Sessions.objects.get(session_id=session_id)
-        except:
-            if session_id:
-                models.Sessions.objects.create(session_id=session_id)
+        if session_id:
+            models.Sessions.objects.get_or_create(session_id=session_id)
 
         return response
 
